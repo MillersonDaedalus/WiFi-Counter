3d6ba15810658c9021919d69766d61cea53af0ce
MillersonDaedalus <tom.millerson@proton.me>
2025-07-21 11:28:06 -0600

    Moved static files to persistent storage folder

--- wifi_counter/settings.py
+++ wifi_counter/settings.py
@@ -117,8 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'static_root/'
+STATIC_URL = 'data/static/'
+STATIC_ROOT = 'data/static_root/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
