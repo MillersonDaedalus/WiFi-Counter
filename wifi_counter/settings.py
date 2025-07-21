604d0399c4d1032a2be207574ad24de96c40097d
MillersonDaedalus <tom.millerson@proton.me>
2025-07-21 11:09:56 -0600

    Updated environment variable names

--- wifi_counter/settings.py
+++ wifi_counter/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS","127.0.0.1").split(",")
 
 
 # Application definition
